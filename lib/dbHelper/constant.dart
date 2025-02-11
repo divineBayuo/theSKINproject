6cd5114c90e54fa58b1b0a612b3106d612560825
Divine Bayuo <divinebayuo@MacBook-Pro.local>
2025-02-11 13:18:43 +0000

    Change the uri connection string

--- lib/dbHelper/constant.dart
+++ lib/dbHelper/constant.dart
@@ -1,7 +1,7 @@
 // user = demo_user
 // password = 1plw68GhUkTYkk9L
 // const MONGO_CONN_URL = "mongodb+srv://demo_user:1plw68GhUkTYkk9L@cluster0.mxkn0zh.mongodb.net/SkinAPPdb?authSource=admin&retryWrites=true&w=majority&appName=Cluster0";
-const MONGO_CONN_URL = "mongodb+srv://divinebayuo:qGV7BI3VEvYjJLfG@cluster0.rekvqoi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const MONGO_CONN_URL = "mongodb+srv://divinebayuo:sRr8uBmW5HjVW8go@cluster0.rekvqoi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 //const MONGO_CONN_URL = "mongodb+srv://demo_user:1plw68GhUkTYkk9L@cluster0.mxkn0zh.mongodb.net/SkinAPPdb?retryWrites=true&w=majority&appName=Cluster0";
 const USER_COLLECTION = "patients"; //name of collection
 // const MONGO_CONN_URL2 = "mongodb+srv://<divinewb123>:<Cyy^Uhp&4>bhZC\">@cluster0.mxkn0zh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
